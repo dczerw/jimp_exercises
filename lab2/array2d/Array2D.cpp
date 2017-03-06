a1ce7b5d7c7a832a642233f38cf2d8311f9dade6
dczerw <dawidosik11@gmail.com>
2017-03-06 20:50:20 +0100

    labki1 rozklepane

---
+++ lab2/array2d/Array2D.cpp
@@ -0,0 +1,43 @@
+//
+// Created by dawid on 06.03.17.
+//
+
+#include "Array2D.h"
+#include <iostream>;
+using namespace std;
+int **Array2D(int n_rows, int n_columns)
+{
+    int **tab = new int*[n_columns];
+
+
+    for(int i=0;i<n_columns;i++)
+    {
+        tab[i]= new int[n_rows];
+    }
+    for (int i = 0; i < n_rows; i++) {
+        for (int j = 0; j < n_columns; j++) {
+            *(*(tab+j)+i)=(j+1)*(i+1);
+
+        }
+    }
+    return tab;
+}
+void Print(int **tab, int n_columns, int n_rows)
+{
+
+    for (int i = 0; i < n_rows; i++) {
+        for (int j = 0; j < n_columns; j++) {
+            cout<<*(*(tab+j)+i)<<" ";
+
+        }
+        cout<<endl;
+    }
+}
+void DeleteArray2D(int **array, int n_rows, int n_columns)
+{
+    for(int i=0;i<n_columns;i++)
+    {
+        delete [] array[i];
+    }
+    delete [] array;
+}
---
+++ lab2/greatestproduct/GreatestProduct.cpp
@@ -0,0 +1,21 @@
+//
+// Created by dawid on 06.03.17.
+//
+
+#include "GreatestProduct.h"
+int GreatestProduct(const std::vector<int> &numbers, int k)
+{
+    vector<int> another_vector={0,0,0};
+    for(int i=0;i<numbers.size();i++)
+    {
+        for(int j=i+1;j<numbers.size();j++)
+        {
+            if(numbers[j]*numbers[i]>another_vector[2]) {
+                another_vector[0] = numbers[i];
+                another_vector[1] = numbers[j];
+                another_vector[2] = numbers[i] * numbers[j];
+            }
+        }
+    }
+    return another_vector[2];
+}
