9e50541d594796ca6537753fd806631fb8683c21
kuba <tom7177@gmail.com>
2017-03-13 00:29:21 +0100

    Polybius obsługuje czytanie nazw plików z konsoli, przechodzi testy step1 i step2

--- lab2/polybius/main.cpp
+++ lab2/polybius/main.cpp
@@ -12,14 +12,21 @@ int main(int argc, char** argv)
 {
 
 
-    ifstream file_read("file1.txt");
-    ofstream file_save ("file2.txt", ios_base::in | ios_base::app);
+    string filename_read=argv[1];
+    string filename_save=argv[2];
+    string operation = argv[3];
+    ifstream file_read(filename_read);
+    ofstream file_save (filename_save, ios_base::in | ios_base::app);
     string word;
     string saved_file;
 
     if(!file_read or !file_save) cout << "Nie można otworzyć pliku!" << endl;
 
-    while(file_read >> word) file_save<<PolybiusCrypt(word);
+    while(file_read >> word)
+    {
+        if(operation=="1") file_save<<PolybiusCrypt(word);
+        else file_save<<PolybiusDecrypt(word);
+    }
 
     file_read.close();
     file_save.close();
