b0f87e4aad32b5a8283583ae73d6492a20f5e103
dczerw <dawidosik11@gmail.com>
2017-03-13 10:31:11 +0100

    ostatni program niby działa

--- lab2/xorcypherbreaker/XorCypherBreaker.h
+++ lab2/xorcypherbreaker/XorCypherBreaker.h
@@ -1,8 +1,15 @@
 //
 // Created by dawid on 13.03.17.
 //
-
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 #ifndef JIMP_EXERCISES_XORCYPHERBREAKER_H
 #define JIMP_EXERCISES_XORCYPHERBREAKER_H
-
+std::string XorCypherBreaker(const vector<char> &cryptogram,
+                             int key_length,
+                             const vector<string> &dictionary);
+void PrintVector(const vector<char> &v);
 #endif //JIMP_EXERCISES_XORCYPHERBREAKER_H
