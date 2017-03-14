9c640385dbe5aa230c7497fd0bbde27b553fa42a
dczerw <dawidosik11@gmail.com>
2017-03-14 13:35:13 +0100

    Dzialajacy  program na plusy z lab3

--- lab3/minimaltimedifference/MinimalTimeDifference.h
+++ lab3/minimaltimedifference/MinimalTimeDifference.h
@@ -2,7 +2,22 @@
 // Created by dawid on 14.03.17.
 //
 
+
+
+#include <vector>
+#include <sstream>
+#include <regex>
+#include <cmath>
+#include <iostream>
+#include<string>
+using namespace std;
+
 #ifndef JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H
 #define JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H
+namespace minimaltimedifference {
+    unsigned int ToMinutes(std::string time_HH_MM);
 
+    unsigned int MinimalTimeDifference(std::vector<std::string> times);
+    int StringToInt(string time);
+}
 #endif //JIMP_EXERCISES_MINIMALTIMEDIFFERENCE_H
