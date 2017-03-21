191dfb5df82f1578c27cedde177f4e78b0d0c788
kuba <tom7177@gmail.com>
2017-03-21 17:26:19 +0100

    Porzadki

--- lab3/tinyurl/TinyUrl.h
+++ lab3/tinyurl/TinyUrl.h
@@ -27,8 +27,7 @@ namespace tinyurl {
     std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
 
     std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
-
-    int GetDecimalFrom62System(std::unique_ptr<TinyUrlCodec> *codec);
+    
 
 }
 
