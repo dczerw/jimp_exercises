1a4c8220c71f0af58d32d4c1ead4a6227a7c687f
kuba <tom7177@gmail.com>
2017-03-28 13:42:21 +0200

    SimpleJson dziala przy testowych wywolaniach w main, ale nie przechodzi testow

--- lab4/netsjson/SimpleJson.cpp
+++ lab4/netsjson/SimpleJson.cpp
@@ -8,50 +8,96 @@ namespace nets
 {
     JsonValue::JsonValue(const int &value) {
     value_int=value;
-        type="int";
+        type=1;
     }
 
-    JsonValue::JsonValue(const float &value) {
-        //value_float_.emplace("None", value);
-        //type="float";
+    JsonValue::JsonValue(const double &value) {
+        value_double=value;
+        type=2;
     }
 
-    JsonValue::JsonValue(const std::string &value) {
-        //value_string_.emplace("None", value);
-        //type="string";
+    JsonValue::JsonValue(const std::string value) {
+        value_string=value;
+        int i;
+        type=3;
 
     }
 
     JsonValue::JsonValue(const bool &value) {
-        //value_bool_.emplace("None", value);
-        //type="bool";
+        value_bool=value;
+        type=4;
 
     }
 
-    /*JsonValue::JsonValue(const nets::JsonValue &value) {
-        value_Json_.emplace(value);
-        type="Json";
 
-    }*/
+    JsonValue::JsonValue(const std::vector<nets::JsonValue> &value) {
+        value_vector=value;
+        type=5;
+    }
 
-    /*JsonValue::JsonValue(std::map<std::string, nets::JsonValue> &value) {
-        value_map_.emplace(value);
-        type="map";
-    }*/
+    JsonValue::JsonValue(std::map<std::string, nets::JsonValue> value) {
+        value_map=value;
+        type=6;
+    }
 
     std::experimental::optional<JsonValue> JsonValue::ValueByName(const std::string &name) const {
         return std::experimental::optional<JsonValue>();
     }
 
     std::string JsonValue::ToString() const {
-        std::string new_value;
-        if(type=="int")
+
+        switch(type)
         {
-            std::stringstream ss;
-            ss << value_int;
-            new_value = ss.str();
+            case 1:
+            {
+                return std::to_string(value_int);
+            }
+            case 2:
+            {
+                std::ostringstream strs;
+                strs << value_double;
+                std::string str = strs.str();
+                return str;
+            }
+            case 3:
+            {
+                std::string correct;
+                int i=0;
+                while(value_string[i]!='\0')
+                {
+                    //correct+=value_string[i];
+                    if(value_string[i]=='\\') correct+='\\';
+                    if(value_string[i]=='\"') correct+='\\';
+                    correct+=value_string[i];
+                    i++;
+                }
+
+
+                return correct;
+            }
+            case 4:
+            {
+                if(value_bool==true) return "true";
+                else return "false";
+            }
+            case 5:
+            {
+                std::string correct = "[";
+                for(int i=0; i<value_vector.size()-1;i++)
+                {
+                    correct+=value_vector[i].ToString();
+                    correct+=", ";
+                }
+                correct+=value_vector[value_vector.size()-1].ToString()+"]";
+                return correct;
+            }
+            default:
+            {
+                return "";
+            }
         }
-        return new_value;
-        //return std::__cxx11::string();
+
+        return std::__cxx11::string();
     }
+
 }
